3f8373c9f250e1a5602fccc198876e2eead09db8
Francisco Soto <francsmojica@gmail.com>
2021-07-13 22:01:16 -0500

    solucion de los ejercicios 1,2,3 y 4

--- src/main/java/minsait/ttaa/datio/common/naming/PlayerOutput.java
+++ src/main/java/minsait/ttaa/datio/common/naming/PlayerOutput.java
@@ -3,5 +3,8 @@ package minsait.ttaa.datio.common.naming;
 public final class PlayerOutput {
 
     public static Field catHeightByPosition = new Field("cat_height_by_position");
-
+    
+    public static Field playerCat = new Field("player_cat");
+    
+    
 }
--- src/main/java/minsait/ttaa/datio/engine/Transformer.java
+++ src/main/java/minsait/ttaa/datio/engine/Transformer.java
@@ -24,10 +24,13 @@ public class Transformer extends Writer {
 
         df = cleanData(df);
         df = exampleWindowFunction(df);
+        df = rankOverWindow(df);
+        df = potentialVsOverall(df);
+        df = twoFilters(df);
         df = columnSelection(df);
-
+        
         // for show 100 records after your transformations and show the Dataset schema
-        df.show(100, false);
+        df.show(210, false);
         df.printSchema();
 
         // Uncomment when you want write your final output
@@ -36,11 +39,19 @@ public class Transformer extends Writer {
 
     private Dataset<Row> columnSelection(Dataset<Row> df) {
         return df.select(
-                shortName.column(),
-                overall.column(),
+        		shortName.column(),
+                longName.column(),
+                age.column(),
                 heightCm.column(),
+                weightKg.column(),
+                nationality.column(),
+                clubName.column(),
+                overall.column(),
+                potential.column(),
                 teamPosition.column(),
-                catHeightByPosition.column()
+                catHeightByPosition.column(),     
+                playerCat.column(),
+                potentialVsOverall.column()
         );
     }
 
@@ -95,5 +106,50 @@ public class Transformer extends Writer {
 
         return df;
     }
+    
+    /* EJERCICIO 2 */
+    private Dataset<Row> rankOverWindow(Dataset<Row> df) {
+        WindowSpec w = Window
+                .partitionBy(nationality.column())
+                .orderBy(overall.column().desc());
 
+        Column rank = rank().over(w);
+
+        Column rule = when(rank.$less(10), "A")
+                .when(rank.$less(20), "B")
+                .when(rank.$less(50), "C")
+                .otherwise("D");
+
+        df = df.withColumn(playerCat.getName(), rule);
+
+        return df;
+    }
+    
+    /* EJERCICIO 3 */
+    private Dataset<Row> potentialVsOverall(Dataset<Row> df) {
+        Column rule_pvso = df.col("overall").divide(df.col("potential"));
+    	
+        df = df.withColumn(potentialVsOverall.getName(), rule_pvso);
+
+        return df;
+    }
+    
+    /* EJERCICIO 4 */
+    private Dataset<Row> twoFilters(Dataset<Row> df) {
+    	Column first = 	df.col("player_cat").equalTo("A");
+    	Column second = df.col("player_cat").equalTo("B");
+        Column third = 	df.col("player_cat").equalTo("C").and( df.col("po_vs_ov").$greater(1.15) );
+        Column fourth = df.col("player_cat").equalTo("D").and( df.col("po_vs_ov").$greater(1.25) );
+        		
+    	Column condition = first.or( second )
+    						 	.or( third )
+    						 	.or( fourth ); 
+    	    	
+        df = df.filter( condition );
+
+        return df;
+    }
+    
+    /* EJERCICIO 5 */   
+    
 }
